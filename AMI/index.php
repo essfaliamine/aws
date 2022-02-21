bd03d4a54bdeb4c79b57cdaaf6acba01ff27b895
essfaliamine <94782338+essfaliamine@users.noreply.github.com>
2022-02-21 15:15:10 +0100

    Update index.php

--- AMI/index.php
+++ AMI/index.php
@@ -1,6 +1,6 @@
 <h3>Hello,</h3>
 <h1>
-<?php echo "This is Static content for Amazon Machine Image"; ?>
+<?php echo "This is Static PHP content for Amazon Machine Image"; ?>
 </h1>
 
 By Mohamed Amine ESSFALI
